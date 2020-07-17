a2fe29131d1a59ea7153c4c9b51e2c5619ff85ca
Rourke Struthers <rourkestruthers@Rourkes-MBP.fios-router.home>
2020-07-17 12:07:11 -0400

    Sprint Complete

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,29 @@
 class RingBuffer:
+
     def __init__(self, capacity):
-        pass
+        # set capacity limit
+        self.capacity = capacity
+        # set index
+        self.index = 0
+        # instantiate empty storage list
+        self.storage = []
 
     def append(self, item):
-        pass
+        # if len of storage is less than capacity
+        if len(self.storage) < self.capacity:
+            # append item
+            self.storage.append(item)
+        # otherwise
+        else:
+            # storage index is equal to that item
+            self.storage[self.index] = item
+        # increase index count by 1
+        self.index += 1
+        # if index and capacity are equal
+        if self.index == self.capacity:
+            # index reset to 0
+            self.index = 0
 
     def get(self):
-        pass
+        # return storage list
+        return self.storage
